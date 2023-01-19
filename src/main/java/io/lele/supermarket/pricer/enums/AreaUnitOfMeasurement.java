74c0a21b0b03da30c092208a0a43cae0131ae17c
Ulrich LELE <lele.ulrich2@gmail.com>
2023-01-19 11:33:47 +0100

    add area based pricing

---
+++ src/main/java/io/lele/supermarket/pricer/enums/AreaUnitOfMeasurement.java
@@ -0,0 +1,31 @@
+package io.lele.supermarket.pricer.enums;
+
+import io.lele.supermarket.pricer.model.UnitOfMeasurement;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum LengthUnitOfMeasurement implements UnitOfMeasurement {
+    Millimeter, Centimeter, Meter, Kilometer;
+
+    @Override
+    public boolean isSIUnit() {
+        return this.equals(Meter) ? true : false;
+    }
+
+    @Override
+    public BigDecimal getConversion() {
+        return conversions.get(this);
+    }
+
+    private static final Map<LengthUnitOfMeasurement, BigDecimal> conversions;
+
+    static {
+        conversions = new HashMap<>();
+        conversions.put(Millimeter, new BigDecimal(1000));
+        conversions.put(Centimeter, new BigDecimal(100));
+        conversions.put(Meter, new BigDecimal(1));
+        conversions.put(Kilometer, new BigDecimal(0.001 ));
+    }
+}
