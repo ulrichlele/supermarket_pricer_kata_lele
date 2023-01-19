ce76971a307573bab95b0f68e3b8eb04e4b823db
Ulrich LELE <lele.ulrich2@gmail.com>
2023-01-19 05:52:44 +0100

    test cases length conversion

--- src/main/java/io/lele/supermarket/pricer/enums/LengthUnitOfMeasurement.java
+++ src/main/java/io/lele/supermarket/pricer/enums/LengthUnitOfMeasurement.java
@@ -1,2 +1,20 @@
-package io.lele.supermarket.pricer.enums;public enum LengthUnitOfMeasurement {
+package io.lele.supermarket.pricer.enums;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum LengthUnitOfMeasurement implements UnitOfMeasurement{
+    Millimeter, Centimeter, Meter, Kilometer;
+
+
+    @Override
+    public boolean isSIUnit() {
+        return false;
+    }
+
+    @Override
+    public BigDecimal getConversion() {
+        return null;
+    }
 }
--- src/test/java/io/lele/supermarket/pricer/service/UnitConverterTest.java
+++ src/test/java/io/lele/supermarket/pricer/service/UnitConverterTest.java
@@ -1,5 +1,6 @@
 package io.lele.supermarket.pricer.service;
 
+import io.lele.supermarket.pricer.enums.LengthUnitOfMeasurement;
 import io.lele.supermarket.pricer.enums.UnitOfMeasurement;
 import io.lele.supermarket.pricer.model.BasketItem;
 import io.lele.supermarket.pricer.model.Product;
@@ -24,9 +25,44 @@ class UnitConverterTest {
     }
 
     @Test
-    @DisplayName("Convert - Flat Amt- exp 45, Qty = 3, UP=15")
-    void concert(){
+    @DisplayName("Should return true if Meter is Si UNit of length")
+    void checkMeterIsSiUnitOfLength(){
+        UnitOfMeasurement unit = LengthUnitOfMeasurement.Meter;
+        assertTrue(unit.isSIUnit());
     }
 
+    @Test
+    @DisplayName("Should return a conversion of 1 as Meter is Si unit of length")
+    void OneIsConversionOfMeterAsSIUnitOfLength(){
+        UnitOfMeasurement unit = LengthUnitOfMeasurement.Meter;
+        assertEquals(BigDecimal.ONE, unit.getConversion());
+    }
+
+    @Test
+    @DisplayName("Conv 10cm to m exp 0.1m")
+    void concert10CentimeterToMetter(){
+        BigDecimal initialValue = new BigDecimal(10);
+        UnitOfMeasurement  initialUnit = LengthUnitOfMeasurement.Centimeter;
+        UnitOfMeasurement finalUnit = LengthUnitOfMeasurement.Meter;
+        assertEquals(new BigDecimal(0.1), converter.convert(initialValue,initialUnit, finalUnit));
+    }
+
+    @Test
+    @DisplayName("Conv 15m to mm exp 15000mm")
+    void concert15MerterToMillimeter(){
+        BigDecimal initialValue = new BigDecimal(15);
+        UnitOfMeasurement  initialUnit = LengthUnitOfMeasurement.Meter;
+        UnitOfMeasurement finalUnit = LengthUnitOfMeasurement.Millimeter;
+        assertEquals(new BigDecimal(15000), converter.convert(initialValue,initialUnit, finalUnit));
+    }
+
+    @Test
+    @DisplayName("Conv 4000cm to km exp 0,04km")
+    void concert4000CentimeterToKilometter(){
+        BigDecimal initialValue = new BigDecimal(4000);
+        UnitOfMeasurement  initialUnit = LengthUnitOfMeasurement.Centimeter;
+        UnitOfMeasurement finalUnit = LengthUnitOfMeasurement.Kilometer;
+        assertEquals(new BigDecimal(0.04), converter.convert(initialValue,initialUnit, finalUnit));
+    }
 
 }
