47aa4b933686bd436c6b3854c52d4d0db5f004d6
Ulrich LELE <lele.ulrich2@gmail.com>
2023-02-07 10:16:47 +0100

    test case promo 10meters bought 2meters offered

--- src/test/java/io/lele/supermarket/pricer/service/PromotionServiceTest.java
+++ src/test/java/io/lele/supermarket/pricer/service/PromotionServiceTest.java
@@ -3,9 +3,7 @@ package io.lele.supermarket.pricer.service;
 import io.lele.supermarket.pricer.exceptions.IncompatibleUnitsException;
 import io.lele.supermarket.pricer.exceptions.InvalidProductPromotion;
 import io.lele.supermarket.pricer.model.*;
-import io.lele.supermarket.pricer.model.enums.LengthUnitOfMeasurement;
-import io.lele.supermarket.pricer.model.enums.PromotionOfferType;
-import io.lele.supermarket.pricer.model.enums.PromotionEvaluationType;
+import io.lele.supermarket.pricer.model.enums.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -98,6 +96,23 @@ public class PromotionServiceTest {
         productService.evaluatePrice(item);
         assertThrows(InvalidProductPromotion.class, () -> promotionService.evaluateProductPromotion(item));
     }
- 
+
+
+    //Involves units of measurements
+    @Test
+    @DisplayName("Ten meters bought two meters offered")
+    void evaluateTenMetersBoughtTwoMetersOffered() throws IncompatibleUnitsException, InvalidProductPromotion {
+        Product product = new Product("Tissue", new BigDecimal(10), null, PricingType.PricePerUnitOfMeasurement, LengthUnitOfMeasurement.Meter);
+        Promotion promotion = new Promotion(PromotionEvaluationType.PurchasedQuantity,  new BigDecimal(10), PromotionOfferType.Quantity, new BigDecimal(2));
+        product.setPromotion(promotion);
+        Basket basket = new Basket();
+        BasketItem item  = new BasketItem(basket, product, new BigDecimal(14), PhysicalQuantity.Length, LengthUnitOfMeasurement.Meter);
+        productService.evaluatePrice(item);
+        promotionService.evaluateProductPromotion(item);
+        assertAll( () -> assertEquals(new BigDecimal(2), item.getOfferedQuantity(), "One product offered"),
+                () -> assertEquals(new BigDecimal(16), item.getTotalQuantity(), "Total quantity increased by one"),
+                () -> assertEquals(new BigDecimal(140), item.getPrice(), "Price is 140 (without promotion)"),
+                () -> assertEquals(new BigDecimal(140), item.getTotalPrice(), "Total price unchanged after promotion"));
+    }
 
 }
